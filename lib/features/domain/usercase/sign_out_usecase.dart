3a45d662f5a96999913340326ec7adcca2c9e105
lytandat632001 <lytandat632001@gmail.com>
2025-07-31 09:31:02 +0700

    Update Domain Layer and Usescases

---
+++ lib/features/domain/usercase/sign_out_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerceapp/core/error/failures.dart';
+import 'package:ecommerceapp/features/auth/data/repositories/auth_repository.dart';
+
+// Use case cho việc đăng xuất
+class SignOutUseCase {
+  final AuthRepository repository;
+
+  SignOutUseCase(this.repository);
+
+  Future<Either<Failure, void>> call() async {
+    return await repository.signOut(); // Gọi phương thức signOut từ AuthRepository
+  }
+}
